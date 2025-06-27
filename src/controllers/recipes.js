cd79d96bf2ba967b87180b69ae174102a38b43b8
Khrystyna Pilip <tina.hristi@gmail.com>
2025-06-27 15:22:38 +0200

    favorites

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -1,4 +1,10 @@
-import { getAllRecipes } from '../services/recipes.js';
+import createHttpError from 'http-errors';
+import {
+  addFavorite,
+  deleteFavorite,
+  getAllFavorites,
+  getAllRecipes,
+} from '../services/recipes.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
@@ -18,5 +24,38 @@ export const getAllRecipesController = async (req, res) => {
   res.status(200).json({
     status: 200,
     data,
+  });
+};
+
+export const addFavoriteController = async (req, res) => {
+  const favorite = await addFavorite(req.user.id, req.params.recipeId);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully added to favorite recipes!`,
+    data: favorite,
+  });
+};
+
+export const deleteFavoriteController = async (req, res, next) => {
+  const { recipeId } = req.params;
+
+  const favourite = await deleteFavorite(req.user.id, recipeId);
+
+  if (!favourite) {
+    next(createHttpError(404, 'Recipe not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const getAllFavoritesController = async (req, res) => {
+  const favorites = await getAllFavorites(req.user.id);
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found favorite recipes!',
+    data: favorites,
   });
 };
