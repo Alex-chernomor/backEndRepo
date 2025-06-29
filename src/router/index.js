294dee2db918d00e064bca038ae5074ccbf04fd1
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-06-29 11:27:58 +0200

    corrected

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,11 @@ import { Router } from 'express';
 import recipesRouter from './recipes.js';
 import authRouter from './auth.js';
 import ingredientsRouter from './ingredients.js';
-// треба буде використати цю міделвару в роуті авторизації auth
-//* import { auth } from '../middlewares/auth.js';
 
 const router = Router();
 
-router.use('/api/recipes', recipesRouter);
-router.use('/api/auth', authRouter);
-router.use('/api', ingredientsRouter);
+router.use('/recipes', recipesRouter);
+router.use('/auth', authRouter);
+router.use('/ingredients', ingredientsRouter);
 
 export default router;
