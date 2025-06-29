276d8fcd67e56950aa8f9720862956ab81429880
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-06-29 15:43:35 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+
+import usersRouter from './users.js';
 import recipesRouter from './recipes.js';
 import authRouter from './auth.js';
 import ingredientsRouter from './ingredients.js';
 
 const router = Router();
 
+router.use('/users', usersRouter);
 router.use('/recipes', recipesRouter);
 router.use('/auth', authRouter);
 router.use('/ingredients', ingredientsRouter);
