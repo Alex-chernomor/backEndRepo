e4679116aeac0e6c58dd130d78ed6e2cd429d760
Sofiia Novychenko <milakotiy@gmail.com>
2025-06-29 13:05:40 +0200

    chenge /ingredients to / in ingredients router

--- src/router/ingredients.js
+++ src/router/ingredients.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import {getAllIngredientsController} from '../controllers/ingredients.js'
+import { getAllIngredientsController } from '../controllers/ingredients.js';
 
 const router = express.Router();
 
-router.get('/ingredients', ctrlWrapper(getAllIngredientsController));
+router.get('/', ctrlWrapper(getAllIngredientsController));
 
-export default router
+export default router;
