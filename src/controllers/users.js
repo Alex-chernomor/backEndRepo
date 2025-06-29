c87c8d0a64874d9b56b35b0614e9dcdbe2f677ea
Hlib <gleb.nikitin18@gmail.com>
2025-06-29 17:39:53 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,17 +1,11 @@
-
-import createHttpError from 'http-errors';
-
 import { User } from '../models/user.js';
+import createHttpError from 'http-errors';
 
 export const getUserById = async (req, res, next) => {
   try {
-
     const { userId } = req.params;
 
-    const userId = req.user._id;
-    const user = await currentUser(userId, User);
-
-    const user = await UserCollection.findById(userId).select('-password');
+    const user = await User.findById(userId).select('-password');
 
     if (!user) {
       throw createHttpError(404, 'User not found');
