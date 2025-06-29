18ef4eca911ae84e56513dfa4e0d71d018a948fe
Khrystyna Pilip <tina.hristi@gmail.com>
2025-06-29 17:32:31 +0200

    Fix bugs

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import recipesRouter from './recipes.js';
 import authRouter from './auth.js';
 import ingredientsRouter from './ingredients.js';
+import usersRouter from './users.js';
 
 const router = Router();
 
 router.use('/recipes', recipesRouter);
 router.use('/auth', authRouter);
 router.use('/ingredients', ingredientsRouter);
+router.use('/users', usersRouter);
 
 export default router;
--- src/router/users.js
+++ src/router/users.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { currentUserController } from '../controllers/users.js';
 import { auth } from '../middlewares/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { isValidId } from '../middlewares/validateObjectId.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import {
   addFavoriteController,
   deleteFavoriteController,
@@ -11,31 +11,23 @@ import {
 } from '../controllers/recipes.js';
 
 const router = Router();
-const jsonParser = express.json();
 
 router.get('/current', auth, currentUserController);
 
 router.post(
   '/favorites/:recipeId',
-  isValidId,
-  jsonParser,
   auth,
+  isValidId,
   ctrlWrapper(addFavoriteController),
 );
 
 router.delete(
   '/favorites/:recipeId',
-  isValidId,
-  jsonParser,
   auth,
+  isValidId,
   ctrlWrapper(deleteFavoriteController),
 );
 
-router.get(
-  '/favorites',
-  jsonParser,
-  auth,
-  ctrlWrapper(getAllFavoritesController),
-);
+router.get('/favorites', auth, ctrlWrapper(getAllFavoritesController));
 
 export default router;
